b0906d296a4c75c57db74d08faccb6c63e97cc11
Arthur Song <arthursong14@gmail.com>
2020-10-08 23:31:59 -0700

    d

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const uuidv4 = require('uuid/v4');
+const uuidv4 = require('uuidv4');
 
 const Note = require('../models/Note');
 const Notebook = require('../models/Notebook');
