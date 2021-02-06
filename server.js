8698ccc2142e17bc8f8cc7cdc8b3cebf2f2f2ab7
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-02-05 19:53:46 -0800

    Try new origin array

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000", "https://www.mwriter.app", "https://mwriter.app"]
+  // origin: "yourmom.com",
+  origin: ["http://localhost:3000", "http://127.0.0.1:3000", "https://www.mwriter.app", "https://mwriter.app", /\.mwriter\.app$/]
 }));
 
 app.post('/image', upload.single('file'), (req, res) => {
